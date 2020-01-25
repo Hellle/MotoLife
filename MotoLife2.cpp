a9f4ff642cfc3d1ee2a57732ca9b25e483b7e8f2
Helmut <hezu@mailbox.org>
2020-01-25 17:27:34 +0100

    Ausgabe Zahl x

--- MotoLife2.cpp
+++ MotoLife2.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+
 using namespace std;
 
 int main(int argc, char **argv) {
+
+	int x=0;
+
 	cout << "Hello world";
-	cout << "Test";
+	cout << "Test"<<endl;
+
+	cin >> x;
+
+	cout <<"Zahl: "<<x<<endl;
+
 	return 0;
 }
